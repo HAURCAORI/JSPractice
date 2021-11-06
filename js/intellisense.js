8aba07370e9a706a83b64302f78bac328b427e11
Guest <guest-vl1qut@h25033.sgb.skir.kr>
2021-11-06 19:07:32 +0900

    vscode

---
+++ js/intellisense.js
@@ -0,0 +1,63 @@
+const javascript = {
+    object : 
+    ["array","boolean","date","math","number","string","regexp","window","navigator","document","image"],
+    common :
+    {
+    },
+    array :{
+        methods : [
+            "reverse()","concat()","sort()","push()","pop()","shift()","join()","indexOf()","lastIndexOf()","firstIndexOf()"
+        ],
+        properties : [
+            "length", "constructor","prototype"
+        ]
+    },
+    boolean :{
+        methods : [
+            "toSource()","toString()","valueOf()"
+        ],
+        properties : [
+            "constructor","prototype"
+        ]
+    },
+    date :{
+        methods : [
+            "Date()","getDate()","getDay()","getFullYear()","getHours()","getMilliseconds()","getMinute()","getMonth()","getTime()","setDate()","setHours()","setMilliseconds()","setTime()"
+        ],
+        properties : [
+            "constructor","prototype"
+        ]
+    },
+    math :{
+        methods : [
+            "max(,)","min(,)","round()","ceil()","floor()","exp()","pow(,)","abs()","random()","sqrt()","sin()","cos()"
+        ],
+        properties : [
+            "PI","E","LN2","LN10","LOG2E","LOG10E","SQRT2","SQRT1_2"
+        ]
+    },
+    number :{
+        methods : [
+            "toExponential()","toFixed()","toLocaleString()","toPrecision()","toString()","valueOf()"
+        ],
+        properties : [
+            "constructor","prototype","MAX_VALUE","MIN_VALUE","NEGATIVE_INFINITY","POSITIVE_INFINITY"
+        ]
+    },
+    string :{
+        methods : [
+            
+        ],
+        properties : [
+            
+        ]
+    },
+    regexp :{
+        methods : [
+            
+        ],
+        properties : [
+            
+        ]
+    }
+};
